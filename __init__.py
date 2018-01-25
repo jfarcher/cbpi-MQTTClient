e6df9d8e625635e0ff48a792e1be88c8d0245c91
peakMeissner <35743804+peakMeissner@users.noreply.github.com>
2018-01-25 13:56:00 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,20 @@
+import json
+from modules import cbpi
+from MQTTClient import MQTTClient
+
+mqttc = MQTTClient()                                                      # initialize the MQTT client
+'''
+@cbpi.backgroundtask(key='mqtt_client', interval=2.5)                     # create bg job with an interval of 2.5 seconds 
+def mqtt_client_background_task(api):
+    sensors = cbpi.cache.get('sensors')                                   # read available sensors
+
+    for key, value in sensors.iteritems():                                # loop over the sensors
+        topic = 'CraftBeerPi/sensor/' + str(value.instance.id)            # define the MQTT topic
+        data = {                                                          # define the playload
+            'id': value.instance.id,
+            'value': value.instance.last_value,
+            'unit': value.instance.get_unit()
+        }
+        payload = json.dumps(data, ensure_ascii=False)                    # convert payload to JSON
+        mqttc.connect().publish(topic, payload)                           # connect to the MQTT server and publish the payload
+'''
